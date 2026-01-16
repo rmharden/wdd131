2de0c22189b664fc7fe22058b00723463eeea538
rmharden <har22079@byui.edu>
2026-01-16 01:17:49 -0500

    following the video, it doesn't match the ponder assgnment in class.

--- ponder/variables-constants.js
+++ ponder/variables-constants.js
@@ -19,3 +19,12 @@ console.log(result);
 
 result = one + Number(two);
 console.log(result);
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course); //works just fine, course is global
+    console.log(student) //works just fine, it's beeing access wi
+}
+console.log(course); // works fine, course is global
+console.log(student); //does not work, can't access a block variable
