76e46955fb86a414ea675b46aadcbf2d1d4c8637
rmharden <har22079@byui.edu>
2026-01-16 20:46:13 -0500

    changed the text for topics to purple by using GetElementById

--- ponder/dom.js
+++ ponder/dom.js
@@ -7,3 +7,5 @@ title.textContent = 'Web Page Components';
 let topics = document.querySelector('#topics');
 
 // document.querySelector('#topic').style.color = 'red'; 
+
+document.getElementById('topics').style.color = 'purple';
