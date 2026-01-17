46858186b7eeb81a9f5161f5101788feb1918e71
rmharden <har22079@byui.edu>
2026-01-16 20:44:33 -0500

    Following the video added style red.

--- ponder/dom.js
+++ ponder/dom.js
@@ -3,3 +3,7 @@ const title = document.querySelector('h1');
 console.log(title);
 
 title.textContent = 'Web Page Components';
+
+let topics = document.querySelector('#topics');
+
+// document.querySelector('#topic').style.color = 'red'; 
