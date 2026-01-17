99658eec7e0905c94c8d0bcc8f11ceb9271f0c55
rmharden <har22079@byui.edu>
2026-01-17 18:07:55 -0500

    commented out document.queryselector

--- ponder/dom.js
+++ ponder/dom.js
@@ -19,3 +19,6 @@ let para = document.querySelector('p');
 // para.style.backgroundColor = 'lightblue';
 
 para.classList.add('background');
+
+
+// document.querySelector('body').classList.add('background');
