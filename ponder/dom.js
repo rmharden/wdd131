5bdb4c43c21cef2b6028b57f72acfa41911b9b30
rmharden <har22079@byui.edu>
2026-01-17 18:33:55 -0500

    changed the h2 to read for the html

--- ponder/dom.js
+++ ponder/dom.js
@@ -31,4 +31,7 @@ let selectElem = document.getElementById('webdevlist');
 selectElem.addEventListener('change', function(){
     let codeValue = selectElem.value;
     console.log(codeValue);
+    document.getElementById(codeValue).style.color = 'red';
 })
+
+
