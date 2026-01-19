63d607241b14af6cf91225de8d8aba83662e43b9
rmharden <har22079@byui.edu>
2026-01-19 10:47:27 -0500

    Realized my code was in the wrong spot for HTML. I had it in the js file. Moving it to the <body> area, I hope that's where it goes.

--- mission/mission.js
+++ mission/mission.js
@@ -1,11 +1,2 @@
-
-<select>
-
-    <option>Choose Mode</option>
-
-    <option value="dark">Dark</option>
-
-    <option value="light">Light</option>
-
-</select>              
+           
                     
