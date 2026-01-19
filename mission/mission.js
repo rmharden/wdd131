e1186488b91a9c1c4970e7a3b9448e7118121ebd
rmharden <har22079@byui.edu>
2026-01-19 10:52:29 -0500

    Added the provided beginning code.

--- mission/mission.js
+++ mission/mission.js
@@ -1,2 +1,14 @@
-           
-                    
+
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        // code for changes to colors and logo
+    } else {
+        // code for changes to colors and logo
+    }
+}
