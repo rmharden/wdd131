20f348833b2c061a341e4c3808cdbc58b0cb7432
rmharden <har22079@byui.edu>
2026-01-19 10:25:21 -0500

    Confused on how to get the code provided to work. I think I need to go and watch the ponder videos again.

---
+++ mission/mission.js
@@ -0,0 +1,11 @@
+
+<select>
+
+    <option>Choose Mode</option>
+
+    <option value="dark">Dark</option>
+
+    <option value="light">Light</option>
+
+</select>              
+                    
