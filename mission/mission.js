aae56b75291b0222dfd706d756fea968a00d3ead
rmharden <har22079@byui.edu>
2026-01-28 18:52:13 -0500

    fixing document.body

--- mission/mission.js
+++ mission/mission.js
@@ -1,6 +1,5 @@
 /*The example has #theme-select so I changed this here*/
 let selectElem = document.querySelector('#theme-select');
-let pageContent = document.body;
 let logo = document.querySelector('img');
 
 selectElem.addEventListener('change', changeTheme);
@@ -10,11 +9,11 @@ function changeTheme() {
     /*I put === here instead of == because our example had === and not == like the code we copied. I don't know if there is a difference but I was going off of the ponder assignment. */
     if (current === 'dark') {
         // code for changes to colors and logo
-        pageContent.classList.add('dark')
+        document.body.classList.add('dark')
         logo.src = 'images/byui-logo-white.png'
     } else {
         // code for changes to colors and logo
-        pageContent.classList.remove('dark')
+        document.body.classList.remove('dark')
         logo.src = 'images/byu-logo.jpg';
     }
 }
