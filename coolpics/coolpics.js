670406c023d495926db5c024e5ec74032537c094
rmharden <har22079@byui.edu>
2026-02-02 09:54:14 -0500

    Commented out the change because i think it was only for the toggling the "x" in the tutorial and I don't think we need it for the assignment.

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -7,6 +7,6 @@ function toggleMenu() {
     // .hide is a class, but we don't need the . here because we already said it is a class with .classlsit.
     menu.classList.toggle('hide');
     // in the video, this was to change Menu into an x. I don't think the assignment needs this.
-    btn.classList.toggle('change');
+    // btn.classList.toggle('change');
 
 }
