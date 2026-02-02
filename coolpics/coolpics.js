362665141b393ca564bce67555d139f9ec7dccc3
rmharden <har22079@byui.edu>
2026-02-01 19:21:49 -0500

    I got confused, removed the js coding for now. I am going to go through the tutorials again to see how each step can work with the Cool Pics part 2 assignment.

--- coolpics/coolpics.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('.menu-btn');
-const menu = document.querySelector('nav');
-
-btn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    menu.classList.toggle('hide');
-    btn.classList.toggle('change');
-}
