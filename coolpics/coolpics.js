4b937eb85f03d9e3b2de5830f31d9293157884d6
rmharden <har22079@byui.edu>
2026-02-03 18:32:56 -0500

    added js code based on the modal ponder

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,5 +1,9 @@
 const btn = document.querySelector('.menu-btn');
 const menu = document.querySelector('nav');
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
 
 btn.addEventListener('click', toggleMenu);
 
@@ -8,6 +12,25 @@ function toggleMenu() {
     menu.classList.toggle('hide');
     // in the video, this was to change Menu into an x. I don't think the assignment needs this.
     // btn.classList.toggle('change');
-
-
 }
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    
+// Code to show modal  - Use event parameter 'e'   
+    
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
