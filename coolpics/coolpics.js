3d969b992ef7725371a62255d0dd78208ca779cc
rmharden <har22079@byui.edu>
2026-02-04 14:56:12 -0500

    removed comments

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,20 +1,17 @@
-/*targets our elements*/
 const gallery = document.querySelector('.gallery');
 const modal = document.querySelector('dialog');
 const modalImage = modal.querySelector('img');
 const closeButton = modal.querySelector('.close-viewer');
 
 // Event listener for opening the modal
-/*This listens for the user to click inside the gallery. When clicked, openModal function will get called.*/
-/*When we call openModal, it didn't send in a parameter.*/
+
 gallery.addEventListener('click', openModal);
 
-/*The e is an event parameter. */
+
 function openModal(e) {
-    /*This is not showing me anything in the console*/
+
     console.log(e.target);
 
-// Code to show modal  - Use event parameter 'e'   
     const img = e.target;
     const src = img.getAttribute('src');
     const alt = img.getAttribute('alt');
@@ -23,15 +20,13 @@ function openModal(e) {
     modalImage.src = full;
     modalImage.alt = alt;
 
-    /*You don't have access the rest of the page until you close the image or close the modal out.*/
     modal.showModal();
 }
-// Close modal on button click (the close button)
+
 closeButton.addEventListener('click', () => {
     modal.close();
 });
 
-// Close modal if clicking outside the image. This closes if they click on the dialog area itself.
 modal.addEventListener('click', (event) => {
     if (event.target === modal) {
         modal.close();
