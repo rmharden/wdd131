c065fbaa47f516ca0d6dd66f732e96319d5fbe0c
rmharden <har22079@byui.edu>
2026-02-13 18:48:20 -0500

    trying different ways to write lists or li

--- ponder/arrays.js
+++ ponder/arrays.js
@@ -1,11 +1,30 @@
+//.forEach
 const steps = ['one', 'two', 'three'];
 
+// Anomonous function as parameter
 // steps.forEach(function(item) {
 //     console.log(item);
 // })
 
+// calling function as the parameter
 steps.forEach(showSteps);
 
+
+// You don't have to call it "item" it can be called x, or step, or something meaningful.
 function showSteps(item) {
     console.log(item);
 }
+
+// .map
+let myList = document.querySelector('#myList');
+
+const stepsHtml = steps.map(listTemplate);
+
+
+// li element is list with a li tag
+// we could put it in the html with <li> but we want it to be "dynamic" through JavaScript
+function listTemplate(item) {
+    return `<li>${item}</li>`; //'<li>'+item+'</li>
+};
+
+myList.innerHTML = stepsHtml.join('');
