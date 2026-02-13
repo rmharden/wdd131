b6c25018a5b400a4d3c0cf365082bb2a8bca0074
rmharden <har22079@byui.edu>
2026-02-13 17:13:54 -0500

    commented out the first foreach and made a new one to show steps

--- ponder/arrays.js
+++ ponder/arrays.js
@@ -1,5 +1,11 @@
 const steps = ['one', 'two', 'three'];
 
-steps.forEach(function(item) {
+// steps.forEach(function(item) {
+//     console.log(item);
+// })
+
+steps.forEach(showSteps);
+
+function showSteps(item) {
     console.log(item);
-})
+}
