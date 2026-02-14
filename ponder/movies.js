7189cbcd6dbb9c199ccab0bb55cd5dc218975c2e
rmharden <har22079@byui.edu>
2026-02-13 20:16:13 -0500

    made it more dynamic and followed the tutorial.

--- ponder/movies.js
+++ ponder/movies.js
@@ -44,8 +44,10 @@ const movies = [
 
     const movieList = document.querySelector('#movie-list');
     movies.forEach(movie => {
+        const article = document.createElement('article');
+        article.className = 'movie';
 
-        let html = `<article class="movie">
+        let html = `
             <h2>${movie.title}</h2>
             <img src=${movie.imgSrc} alt=${movie.imgAlt}>
             <p><strong>Release Date:</strong> ${movie.date}</p>
@@ -53,7 +55,9 @@ const movies = [
             <p><strong>Genre:</strong> ${movie.genre}</p>
             <p><strong>Rating:</strong> <span aria-label="${movie.stars.length} out of 5 stars" role="img">${movie.stars}</span></p>
             <p id='desc'>${movie.description}</p>
-        </article>`
-movieList.innerHTML += html;
+        `
+    article.innerHTML = html;
+    movieList.appendChild(article);
     
     });
+    
