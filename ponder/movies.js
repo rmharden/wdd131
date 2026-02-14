5a7676dc5457e97d1bfc39c17cc2aa34f98b7db4
rmharden <har22079@byui.edu>
2026-02-13 19:40:15 -0500

    making the articles dynamic, deleted both, moved one to the new js file.

---
+++ ponder/movies.js
@@ -0,0 +1,9 @@
+let html = `<article class="movie">
+            <h2>Spider-Man: Into the Spider-Verse</h2>
+            <img src="https://wddbyui.github.io/wdd131/images/spiderman.png" alt="Miles Morales swinging through the city">
+            <p><strong>Release Date:</strong> Dec 14, 2018</p>
+            <p><strong>Recommended Age:</strong> 10+</p>
+            <p><strong>Genre:</strong> Action/Adventure</p>
+            <p><strong>Rating:</strong> <span aria-label="5 out of 5 stars" role="img">⭐⭐⭐⭐⭐</span></p>
+            <p id='desc'>Miles Morales becomes the Spider-Man of his reality and crosses paths with others from the multiverse.</p>
+        </article>`
