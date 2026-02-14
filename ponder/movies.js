16025b44a55b5e60238eeca5e502567167f73b6f
rmharden <har22079@byui.edu>
2026-02-13 20:03:23 -0500

    changing the foreach so it will find the movies and info

--- ponder/movies.js
+++ ponder/movies.js
@@ -46,10 +46,10 @@ const movies = [
 
         const movieList = document.querySelector('#movie-list');
         let html = `<article class="movie">
-            <h2>Spider-Man: Into the Spider-Verse</h2>
-            <img src="https://wddbyui.github.io/wdd131/images/spiderman.png" alt="Miles Morales swinging through the city">
-            <p><strong>Release Date:</strong> Dec 14, 2018</p>
-            <p><strong>Recommended Age:</strong> 10+</p>
+            <h2>${movie.title}</h2>
+            <img src=${movie.imgSrc} alt=${movie.imgAlt}>
+            <p><strong>Release Date:</strong> ${movie.date}</p>
+            <p><strong>Recommended Age:</strong> ${movie.ages}</p>
             <p><strong>Genre:</strong> Action/Adventure</p>
             <p><strong>Rating:</strong> <span aria-label="5 out of 5 stars" role="img">⭐⭐⭐⭐⭐</span></p>
             <p id='desc'>Miles Morales becomes the Spider-Man of his reality and crosses paths with others from the multiverse.</p>
