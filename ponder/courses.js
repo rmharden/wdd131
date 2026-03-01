c0c76a755c8fbcb3f356a9b63019493dc800b85a
rmharden <har22079@byui.edu>
2026-02-28 20:35:07 -0500

    pasted the code for the method

--- ponder/courses.js
+++ ponder/courses.js
@@ -1,7 +1,16 @@
 const aCourse = {
     code: 'CSE121b',
     name: 'Javascript Language',
-    logo: 'images/js-logo.png'
+    logo: 'images/js-logo.png',
+
+    sections: [
+        { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
+        { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
+        ],
+                
+        enrollStudent: function () {
+                console.log('student enrolled');
+            }           
 };
 
 console.log(aCourse.code);
@@ -9,3 +18,11 @@ console.log(aCourse.name);
 
 document.querySelector('#courseName').textContent = aCourse.name;
 document.querySelector('#courseCode').textContent = aCourse.code;
+
+document.querySelector('img').setAttribute('src', aCourse.logo);
+document.querySelector('img').setAttribute('alt', aCourse.name);
+document.querySelector('img').style.width='100px';
+
+console.log(aCourse.sections[1].roomNum);
+
+aCourse.enrollStudent();
