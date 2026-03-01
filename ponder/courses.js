b68d8265be9aaf8b6468d580b6d39c18a1bf0a79
rmharden <har22079@byui.edu>
2026-02-28 20:20:46 -0500

    fixed the element to be courseCode

--- ponder/courses.js
+++ ponder/courses.js
@@ -1,5 +1,5 @@
 const aCourse = {
-    code: 'CSE2121b',
+    code: 'CSE121b',
     name: 'Javascript Language',
     logo: 'images/js-logo.png'
 };
@@ -8,4 +8,4 @@ console.log(aCourse.code);
 console.log(aCourse.name);
 
 document.querySelector('#courseName').textContent = aCourse.name;
-document.querySelector('#courseName').textContent = aCourse.code;
+document.querySelector('#courseCode').textContent = aCourse.code;
