bd0f63d7869e4cdcd87a5186a7bf10d15f38d7de
rmharden <har22079@byui.edu>
2026-03-09 17:02:20 -0400

    moved the function down

--- ponder/sortfilter.js
+++ ponder/sortfilter.js
@@ -2,16 +2,6 @@ nums = [12, 10, 8, 3];
 
 console.log(nums.sort(compareFn));
 
-
-function compareFn(a,b) {
-  if (a < b) {
-    return -1;
-  } else if (a > b) {
-    return 1;
-  }
- return 0;
-}
-
 const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
                 
 let simpleSort = simpleList.sort();
@@ -34,3 +24,12 @@ function searchFruit(item){
 }
 
 console.log(filterFruit);
+//sorting properties into an object array
+ function compareFn(a,b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  }
+ return 0;
+}
