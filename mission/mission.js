e5632916ec7fc8119f6465082913128c565ffc6b
rmharden <har22079@byui.edu>
2026-04-21 17:22:33 -0400

    I fixed the h2 font color to change and work in js for the mode change now

--- mission/mission.js
+++ mission/mission.js
@@ -1,6 +1,8 @@
 let selectElem = document.querySelector('select');
 let logo = document.querySelector('img');
 let border = document.querySelector('#content')
+let university = document.querySelector('h2');
+let background = document.querySelector()
 
 selectElem.addEventListener('change', changeTheme);
 
@@ -8,10 +10,12 @@ function changeTheme() {
     let current = selectElem.value;
     if (current === 'dark') {
         logo.src ="images/byui-black.png";
-        border.style.border = '1px solid white';
+        border.style.border = '1px solid #cccccc';
+        university.style.color = '#4da3ff';
     }
     else {
         logo.src="images/byui-logo-blue.jpeg";
         border.style.border = '1px solid darkgray';
+        university.style.color = 'steelblue';
     }
 }
