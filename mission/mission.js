8f315c36c98c69414d9efb126ecdbefbc5ed9a69
rmharden <har22079@byui.edu>
2026-04-21 17:40:46 -0400

    I added the change to the font color

--- mission/mission.js
+++ mission/mission.js
@@ -12,12 +12,14 @@ function changeTheme() {
         document.body.style.backgroundColor = 'black'
         logo.src ="images/byui-black.png";
         border.style.border = '1px solid #cccccc';
+        document.body.style.color = "#e0e0e0"
         university.style.color = '#4da3ff';
     }
     else {
         document.body.style.backgroundColor = 'white'
         logo.src="images/byui-logo-blue.jpeg";
         border.style.border = '1px solid darkgray';
+        document.body.style.color = "black"
         university.style.color = 'steelblue';
     }
 }
