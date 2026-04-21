b85ac0ea59590a805fc465f4c27f9ecfa868010a
rmharden <har22079@byui.edu>
2026-04-21 12:18:36 -0400

    learning how to different ways to select ids and change style

--- ponder/dom.js
+++ ponder/dom.js
@@ -4,4 +4,9 @@ console.log(title);
 
 title.textContent = 'Web Page Components';
 
-document.querySelector('#topics').style.color = 'red';
+//document.querySelector('#topics').style.color = 'red';
+let topics = document.querySelector('#topics');
+// topics.style.color = 'red'; 
+
+document.getElementById('topics').style.color = "purple";
+
