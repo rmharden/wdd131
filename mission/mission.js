e1ca3770dd15f5be7e8c09710d2749c0a012feb4
rmharden <har22079@byui.edu>
2026-04-27 14:06:10 -0400

    took out some of the css changes that were in js and added the add and remove. I learned it from out slides for the Documnet Object Model for this week.

--- mission/mission.js
+++ mission/mission.js
@@ -1,25 +1,18 @@
-let selectElem = document.querySelector('select');
+let selectElem = document.querySelector('#theme-select');
 let logo = document.querySelector('#byui-logo');
 let border = document.querySelector('#content')
 let university = document.querySelector('h2');
-let background = document.body;
 
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
     if (current === 'dark') {
-        // document.body.style.backgroundColor = 'black'
-        // logo.src ="images/byui-black2.png";
-        // border.style.border = '1px solid #cccccc';
-        // document.body.style.color = "#e0e0e0"
-        // university.style.color = '#4da3ff';
+        document.body.classList.add('dark')
+        logo.src ="images/byui-black2.png";
     }
     else {
-        document.body.style.backgroundColor = 'white'
+        document.body.classList.remove('dark');
         logo.src="images/byui-logo-blue.jpeg";
-        border.style.border = '1px solid darkgray';
-        document.body.style.color = "black"
-        university.style.color = 'steelblue';
     }
 }
