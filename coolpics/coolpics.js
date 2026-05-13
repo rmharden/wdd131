e190db56d2dc0cf67602e8d5811336d68b85ce02
rmharden <har22079@byui.edu>
2026-05-13 13:47:12 -0400

    removed comments

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -12,7 +12,6 @@ const modal = document.querySelector('dialog');
 const modalImage = modal.querySelector('img');
 const closeButton = modal.querySelector('.close-viewer');
 
-// Event listener for opening the modal
 gallery.addEventListener('click', openModal);
 
 function openModal(e) {
@@ -28,12 +27,9 @@ function openModal(e) {
 
     modal.showModal();    
 }
-// Close modal on button click
 closeButton.addEventListener('click', () => {
     modal.close();
 });
-
-// Close modal if clicking outside the image
 modal.addEventListener('click', (event) => {
     if (event.target === modal) {
         modal.close();
