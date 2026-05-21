0056ffcf9c0f22b27c1490a833ff73b3fcf0dd8a
rmharden <har22079@byui.edu>
2026-05-21 13:08:11 -0400

    added the filter array

--- ponder/arrays.js
+++ ponder/arrays.js
@@ -64,3 +64,11 @@ console.log(totalPoints);
 
 let gpaAverage = totalPoints/gpaPoints.length;
 console.log(gpaAverage);
+
+// .filter array method
+
+const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+
+const shortWords = words.filter(function(word){
+    return word.length < 6;
+})
