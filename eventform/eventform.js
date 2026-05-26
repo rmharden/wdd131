57cdd131d109ce2e7b7817e2419765e1eec6353f
rmharden <har22079@byui.edu>
2026-05-26 18:25:59 -0400

    removed an extra t in the container name

--- eventform/eventform.js
+++ eventform/eventform.js
@@ -1,7 +1,16 @@
 const form = document.querySelector("#eventForm");
 const userType = document.querySelector("#userType");
-const usertTypeContainer = document.querySelector("#usertTypeContainer");
+const userTypeContainer = document.querySelector("#usertTypeContainer");
 const studentId = document.querySelector("#studentId");
 const accessCode = document.querySelector("#accessCode");
 const output = document.querySelector("#output");
 
+function updateUserTypeField() {
+    const value = userType.value;
+
+    if (value === 'studentId') {
+        u
+    }
+
+    )
+}
