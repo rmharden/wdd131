f59d122e9843749fa5e1d3f7938852abb14e4f3a
rmharden <har22079@byui.edu>
2026-05-26 18:22:55 -0400

    added the const for studentId, accessCode, and output

--- eventform/eventform.js
+++ eventform/eventform.js
@@ -1,4 +1,7 @@
 const form = document.querySelector("#eventForm");
 const userType = document.querySelector("#userType");
 const usertTypeContainer = document.querySelector("#usertTypeContainer");
+const studentId = document.querySelector("#studentId");
+const accessCode = document.querySelector("#accessCode");
+const output = document.querySelector("#output");
 
