2f1dfd3727d0b180e4744c55251e14de16a20f5b
rmharden <har22079@byui.edu>
2026-05-26 18:20:28 -0400

    added the const for the usertTypeContainer

--- eventform/eventform.js
+++ eventform/eventform.js
@@ -1,3 +1,4 @@
 const form = document.querySelector("#eventForm");
 const userType = document.querySelector("#userType");
+const usertTypeContainer = document.querySelector("#usertTypeContainer");
 
