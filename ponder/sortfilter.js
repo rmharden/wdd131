ec2f672a665814dfa8c4cd2b4b784f528232501e
rmharden <har22079@byui.edu>
2026-06-16 12:25:58 -0400

    added notes and added the provided compareFn function. Called the compareFn function with the console.log.

--- ponder/sortfilter.js
+++ ponder/sortfilter.js
@@ -1,3 +1,16 @@
 nums = [12, 10, 8, 3];
 
-console.log(nums.sort());
+// This is the new sort that will call the below compare function so it won't sort by first digit, but it compares and sorts by value.
+console.log(nums.sort(compareFn));
+
+// console.log(nums.sort());
+
+//compare function because the sort above will sort based on the first digit of the number not based on its value.
+function compareFn(a,b) {
+    if (a < b) {
+        return -1;
+    } else if (a > b) {
+        return 1;
+    }
+    return 0;
+}
