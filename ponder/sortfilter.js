3a8ecf1d323b2373cc81d3699ac7f1fab4b8c82e
rmharden <har22079@byui.edu>
2026-06-16 12:28:29 -0400

    added the provided string array with the fruit values.

--- ponder/sortfilter.js
+++ ponder/sortfilter.js
@@ -14,3 +14,7 @@ function compareFn(a,b) {
     }
     return 0;
 }
+
+// this is a string array:
+
+const simpleList = ["oranges", "grapes", "lemons", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
