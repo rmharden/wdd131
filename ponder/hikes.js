373fc31bfc5df758d832987bb62a2a4b4c09f220
rmharden <har22079@byui.edu>
2026-06-16 17:12:46 -0400

    added the provided template for each hike.

---
+++ ponder/hikes.js
@@ -0,0 +1,62 @@
+const hikes = [
+    {
+        name: "",
+        stub: "",
+        imgSrc:
+        "https://wdd131.netlify.app/examples/hikes/images",
+        imgAlt: "",
+        distane: "",
+        tags: ["",],
+        difficulty: ,
+        description: "",
+        directions: ""
+    },
+    {
+        name: "",
+        stub: "",
+        imgSrc:
+        "https://wdd131.netlify.app/examples/hikes/images",
+        imgAlt: "",
+        distane: "",
+        tags: ["",],
+        difficulty: ,
+        description: "",
+        directions: ""
+    },
+    {
+        name: "",
+        stub: "",
+        imgSrc:
+        "https://wdd131.netlify.app/examples/hikes/images",
+        imgAlt: "",
+        distane: "",
+        tags: ["",],
+        difficulty: ,
+        description: "",
+        directions: ""
+    },
+    {
+        name: "",
+        stub: "",
+        imgSrc:
+        "https://wdd131.netlify.app/examples/hikes/images",
+        imgAlt: "",
+        distane: "",
+        tags: ["",],
+        difficulty: ,
+        description: "",
+        directions: ""
+    },
+    {
+        name: "",
+        stub: "",
+        imgSrc:
+        "https://wdd131.netlify.app/examples/hikes/images",
+        imgAlt: "",
+        distane: "",
+        tags: ["",],
+        difficulty: ,
+        description: "",
+        directions: ""
+    }
+]
