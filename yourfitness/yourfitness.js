68016f39ef45affae144380f614ab684a38cc7d9
rmharden <har22079@byui.edu>
2026-07-15 15:38:14 -0400

    writing the object arrays for the three generator workout cards

---
+++ yourfitness/yourfitness.js
@@ -0,0 +1,32 @@
+const exercises = [
+    {
+        name: 'Sit-ups',
+        repetitions: '10-30',
+        count: '2',
+        description: 'Keep feet flat on the ground. If needed, a friend can hold your feet down. Place your hands on opposite sholders. Keep your arms and elbows against your chest. Tighten your abdominals and bring your torso up. Touch your elbows or arms to your knees and return to the starting position. No rocking or using momentum to sit up.',
+        image: 'images/',
+        tags: ['sit-ups', 'muscular fitness', 'calesthinics', 'assisted', 'abdominal', 'generator'],
+        group: ['Muscular Fitness', 'Calesthincs', 'Generator'],
+        order: 2
+    },
+    {
+        name: 'Push-ups',
+        repetitions: '5-30',
+        count: '2',
+        description: '',
+        image: 'images/',
+        tags: ['push-ups', 'muscular fitness', 'calesthinics', 'arms', 'generator'],
+        group: ['Muscular Fitness', 'Calesthincs', 'Generator'],
+        order: 4
+    },
+    {
+        name: 'Squats',
+        repetitions: '10',
+        count: '2',
+        description: '',
+        image: 'images/',
+        tags: ['squats', 'muscular fitness', 'calesthinics', 'assisted', 'glutes', 'legs', 'generator'],
+        group: ['Muscular Fitness', 'Calesthincs', 'Generator'],
+        order: 10
+    }
+];
