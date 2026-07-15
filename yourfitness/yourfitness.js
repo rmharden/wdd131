bbbb8c0c5292908f7af6ad0222265c34bd8090c9
rmharden <har22079@byui.edu>
2026-07-15 15:45:22 -0400

    added the three images to js

--- yourfitness/yourfitness.js
+++ yourfitness/yourfitness.js
@@ -4,7 +4,7 @@ const exercises = [
         repetitions: '10-30',
         count: '2',
         description: 'Keep feet flat on the ground. If needed, a friend can hold your feet down. Place your hands on opposite sholders. Keep your arms and elbows against your chest. Tighten your abdominals and bring your torso up. Touch your elbows or arms to your knees and return to the starting position. No rocking or using momentum to sit up.',
-        image: 'images/',
+        image: 'images/grok-imagine-sit-up-001.jpg',
         tags: ['sit-ups', 'muscular fitness', 'calesthinics', 'assisted', 'abdominal', 'generator'],
         group: ['Muscular Fitness', 'Calesthincs', 'Generator'],
         order: 2
@@ -14,7 +14,7 @@ const exercises = [
         repetitions: '5-30',
         count: '2',
         description: '',
-        image: 'images/',
+        image: 'images/pexels-kseniachernaya-7302890.jpg',
         tags: ['push-ups', 'muscular fitness', 'calesthinics', 'arms', 'generator'],
         group: ['Muscular Fitness', 'Calesthincs', 'Generator'],
         order: 4
@@ -24,7 +24,7 @@ const exercises = [
         repetitions: '10',
         count: '2',
         description: '',
-        image: 'images/',
+        image: 'images/pexels-kindelmedia-7935630.jpg',
         tags: ['squats', 'muscular fitness', 'calesthinics', 'assisted', 'glutes', 'legs', 'generator'],
         group: ['Muscular Fitness', 'Calesthincs', 'Generator'],
         order: 10
