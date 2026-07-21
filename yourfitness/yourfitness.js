82fd2bfacfafd86c2666cedb190a5951817635ee
rmharden <har22079@byui.edu>
2026-07-20 20:32:18 -0400

    adding the object array

--- yourfitness/yourfitness.js
+++ yourfitness/yourfitness.js
@@ -7,3 +7,32 @@ function toggleMenu() {
     menu.classList.toggle('hide');
     btn.classList.toggle('change');
 }
+
+
+
+const exercise = [
+    {
+        name: 'Sit-ups',
+        reps: '30',
+        count: '2',
+        description: '',
+        order: '15',
+        tags: ['muscular', 'abdomen', 'sitting', 'assisted']
+    },
+    {
+        name: 'Push-ups',
+        reps: '5-15',
+        count: '2',
+        description: '',
+        order: '17',
+        tags: ['muscular', 'arms']
+    },
+    {
+        name: 'Squats',
+        reps: '10-30',
+        count: '2',
+        description: '',
+        order: '23',
+        tags: ['muscular', 'legs', 'standing']
+    }
+]
