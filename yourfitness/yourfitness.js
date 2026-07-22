4107d97c3dc222ea778a54be074b2c147d2fdbee
rmharden <har22079@byui.edu>
2026-07-22 15:41:40 -0400

    deleted the functions for the hamburger menu

--- yourfitness/yourfitness.js
+++ yourfitness/yourfitness.js
@@ -1,12 +1,3 @@
-const btn = document.querySelector('.menu-btn');
-const menu = document.querySelector('nav');
-
-btn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    menu.classList.toggle('hide');
-    btn.classList.toggle('change');
-}
 const exercises = [
     // Stretching
     {
