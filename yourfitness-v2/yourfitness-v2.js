eb4a9293222e36a7d183b77da384824e4e58349e
rmharden <har22079@byui.edu>
2026-08-13 16:01:29 -0400

    added notes

--- yourfitness-v2/yourfitness-v2.js
+++ yourfitness-v2/yourfitness-v2.js
@@ -398,6 +398,16 @@ function generateWorkout(event) {
         const minutes = time % 60;
 
         const period = hour >= 12 ? "PM" : "AM";
+        /*
+        https://www.w3schools.com/js/js_if_ternary.asp
+        the ? is a tenary operator:
+        let period;
+        if (hour >= 12) {
+            period = "PM";
+        } else {
+            period = "AM";
+        }
+        */
         const displayHour = hour % 12 || 12;
         const displayMinutes = String(minutes).padStart(2, "0");
 
